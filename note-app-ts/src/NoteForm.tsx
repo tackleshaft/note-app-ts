03175eb184d949a212a78b98970518a21a6d1301
Hugh Stapleton <hstapleton808@gmail.com>
2024-04-01 19:02:26 -0400

    useRef added

--- note-app-ts/src/NoteForm.tsx
+++ note-app-ts/src/NoteForm.tsx
@@ -1,7 +1,11 @@
-import { Col, Form, Row, Stack } from "react-bootstrap";
-import Select from 'react-select';
+import { useRef } from "react";
+import { Col, Form, Row, Stack, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Creatable from 'react-select/creatable';
 
 export function NoteForm() {
+    const titleRef = useRef<HTMLInputElement>(null)
+    const markdownRef = useRef<HTMLTextAreaElement>(null)
     return(
         <Form>
           <Stack gap={4}>
@@ -9,16 +13,30 @@ export function NoteForm() {
               <Col>
                 <Form.Group controlId="title">
                   <Form.Label>Title</Form.Label>
-                  <Form.Control required />
+                  <Form.Control ref={titleRef} required />
                 </Form.Group>
               </Col>
               <Col>
                 <Form.Group controlId="tags">
                   <Form.Label>Tags</Form.Label>
-                  <Select isMulti />
+                  <Creatable isMulti />
                 </Form.Group>
               </Col>
             </Row>
+            <Form.Group controlId="markdown">
+                  <Form.Label>Body</Form.Label>
+                  <Form.Control ref={markdownRef} required as="textarea" rows={15} />
+            </Form.Group>
+            <Stack direction="horizontal" gap={2} className="justify-content-end">
+                <Button type="submit" variant="primary">
+                    Save
+                </Button>
+                <Link to="..">
+                  <Button type="submit" variant="outline-secondary">
+                      Cancel
+                  </Button>
+                </Link>
+            </Stack>
           </Stack>
         </Form>
     )
