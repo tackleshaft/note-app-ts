ed296a06c2a56e10703cad35fc82aac9853cfd63
Hugh Stapleton <hstapleton808@gmail.com>
2024-04-02 15:28:46 -0400

    working on notelist

--- note-app-ts/src/NoteList.tsx
+++ note-app-ts/src/NoteList.tsx
@@ -1,3 +1,49 @@
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Select from "react-select";
+
+
 export function NoteList() {
-    return <h1>List</h1>
+
+    return 
+        <>
+            <Row>
+                <Col><h1>Notes</h1></Col>
+                <Col xs="auto">
+                    <Stack gap={2} direction="horizontal">
+                        <Link to="/new">
+                            <Button variant="primary">Create</Button>
+                        </Link>
+                        <Button variant="outline-secondary">Edit Tags</Button>
+                    </Stack>
+                </Col>
+            </Row>
+            <Form>
+                <Row className="mb-4">
+                    <Col>
+                    <Form.Group controlId="title">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" />
+                    </Form.Group>
+                    </Col>
+                    <Col>
+                            <Form.Group controlId="tags">
+                        <Form.Label>Tags</Form.Label>
+                        <Select 
+                            value={selectedTags.map(tag => {
+                                return { label: tag.label, value: tag.id }
+                            })}
+                            options={availableTags.map(tag => {
+                                return { label: tag.label, value: tag.id }
+                            })}
+                            onChange={tags => {
+                                setSelectedTags(tags.map(tag => {
+                                    return { label: tag.label, id: tag.value }
+                                }))
+                        }} isMulti />
+                        </Form.Group>
+                    </Col>
+                </Row>
+            </Form>
+        </>
 }
