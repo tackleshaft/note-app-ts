d8924d3f88a9dab3b84b265f8bde9a584033defe
Hugh Stapleton <hstapleton808@gmail.com>
2024-04-04 16:02:59 -0400

    delete functionality added

--- note-app-ts/src/App.tsx
+++ note-app-ts/src/App.tsx
@@ -52,6 +52,12 @@ function App() {
     })
   }
 
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
   function addTag(tag: Tag) {
     setTags(prev => [...prev, tag])
   }
@@ -81,7 +87,7 @@ function App() {
           />
         }/>
         <Route path="/:id" element={ <NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote}/>} />
           <Route path="edit" element={<EditNote 
           onSubmit={onUpdateNote} 
           onAddTag={addTag} 
