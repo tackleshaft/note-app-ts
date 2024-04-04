d4b9a979716028b8f7626a54c58ab367580eaba5
Hugh Stapleton <hstapleton808@gmail.com>
2024-04-03 21:43:57 -0400

    bootstrap styling

--- note-app-ts/src/NoteList.tsx
+++ note-app-ts/src/NoteList.tsx
@@ -1,8 +1,9 @@
 import { useMemo, useState } from "react";
-import { Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
+import { Badge, Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Select from "react-select";
 import { Note, Tag } from "./App";
+import styles from "./NoteList.module.css"
 
 type NoteListProps = {
     availableTags: Tag[]
@@ -81,10 +82,34 @@ export function NoteList({ availableTags, notes }: NoteListProps) {
     )
 }
 
-function NoteCard({ id, title, tags}: SimplifiedNote) {
-    return <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
+function NoteCard({ id, title, tags }: SimplifiedNote) {
+    return (
+      <Card
+        as={Link}
+        to={`/${id}`}
+        className={`h-100 text-reset text-decoration-none ${styles.card}`}
+      >
         <Card.Body>
-            
+          <Stack
+            gap={2}
+            className="align-items-center justify-content-center h-100"
+          >
+            <span className="fs-5">{title}</span>
+            {tags.length > 0 && (
+              <Stack
+                gap={1}
+                direction="horizontal"
+                className="justify-content-center flex-wrap"
+              >
+                {tags.map(tag => (
+                  <Badge className="text-truncate" key={tag.id}>
+                    {tag.label}
+                  </Badge>
+                ))}
+              </Stack>
+            )}
+          </Stack>
         </Card.Body>
-    </Card>
-}
+      </Card>
+    )
+  }
